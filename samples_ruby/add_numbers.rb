1868bd663f4d28f1f3c9703f74a29884fdc6d0e4
Emily Ring <btyy77c@gmail.com>
2017-09-05 15:54:22 -0400

    Test travis ci

--- samples_ruby/add_numbers.rb
+++ samples_ruby/add_numbers.rb
@@ -5,7 +5,7 @@ second = 2
 # Stuff you need to edit goes after this line. Do not edit this line! -------------
 
 # FIX ME! This variable (first_plus_second) needs to add two variables: (first and second)
-first_plus_second = 2
+first_plus_second = first.to_i + second.to_i
 
 # End stuff you need to edit. Don't change anything after this line --------
 
