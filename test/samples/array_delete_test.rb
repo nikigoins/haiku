abecbd6b352c9c38004b7635c797f5c80f471f87
Emily Ring <btyy77c@gmail.com>
2017-09-12 08:42:56 -0400

    Added array delete

---
+++ test/samples/array_delete_test.rb
@@ -0,0 +1,21 @@
+require 'samples/find_variable'
+
+class AddNumbersTest < FindVariable
+  def setup
+    find_method('samples_ruby/array_delete.rb')
+  end
+
+  test 'remove [0]' do
+    check_method([1, 2, 3], 0, [2, 3])
+  end
+
+  test 'remove [3]' do
+    check_method([0, 1, 2, 3, 4, 5], 3, [0, 1, 2, 4, 5])
+  end
+
+  private def check_method(arr, val, answer)
+    puts "When I delete arr[#{val}], the arry becomes #{answer}"
+    @check_method += "array_delete(#{arr}, #{val})"
+    super(answer)
+  end
+end
