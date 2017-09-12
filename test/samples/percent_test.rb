b1dab9bcdeb059c23a6c10fe0551b6198f098a15
Emily Ring <btyy77c@gmail.com>
2017-09-12 07:58:06 -0400

    Added percent

---
+++ test/samples/percent_test.rb
@@ -0,0 +1,25 @@
+require 'samples/find_variable'
+
+class GradingTest < FindVariable
+  def setup
+    find_method('samples_ruby/percent.rb')
+  end
+
+  test '5/10' do
+    check_method(5, 10, 0.5)
+  end
+
+  test '2/20' do
+    check_method(2, 20, 0.1)
+  end
+
+  test '30/3' do
+    check_method(30, 3, 10.0)
+  end
+
+  private def check_method(num1, num2, answer)
+    puts "#{num1} / #{num2} = #{answer}"
+    @check_method += "percent(#{num1}, #{num2})"
+    super(answer)
+  end
+end
