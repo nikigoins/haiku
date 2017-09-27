45b68ceea8742c4fee1005d75b8ba80585b93c79
Emily Ring <btyy77c@gmail.com>
2017-09-27 10:32:20 +0000

    old counter

--- samples_ruby/syllable_counter.rb
+++ samples_ruby/syllable_counter.rb
@@ -1,7 +1,63 @@
-require 'ruby_rhymes'
+# I used this site http://www.phonicsontheweb.com/syllables.php for syllable rules
+# I don't think it's 100% correct
 
 def syllable_counter(word)
-  return word.to_s.to_phrase.syllables
+  word = word.downcase.split("")
+
+  word = remove_silent_e(word)
+  word = remove_everything_but_vowels(word)
+
+  return word.join("").split(" ").size
 end
 
-puts syllable_counter('hello')
+def remove_silent_e(word)
+  vowels = ["a", "e", "i", "o", "u", "y"]
+  new_word = []
+
+  last_two_values = word[(word.length - 2)..(word.length - 1)]
+
+  if last_two_values[0] != "l" && last_two_values[1] == "e"
+    word.pop
+  end
+
+  if last_two_values == ["e", "d"]
+    word[word.length - 2] = "a"
+  end
+
+  word.each_with_index do |letter, index|
+    if index - 2 >= 0 &&
+       letter == "e" &&
+       vowels.include?(word[index - 2]) &&
+       word[index + 1] != "r"
+      new_word << " "
+    else
+      new_word << letter
+    end
+  end
+
+  return new_word
+end
+
+def remove_everything_but_vowels(word)
+  new_word = []
+
+  word.each do |letter|
+    if letter == "a"
+      new_word << letter
+    elsif letter == "e"
+      new_word << letter
+    elsif letter == "i"
+      new_word << letter
+    elsif letter == "o"
+      new_word << letter
+    elsif letter == "u"
+      new_word << letter
+    elsif letter == "y"
+      new_word << letter
+    else
+      new_word << " "
+    end
+  end
+
+  return new_word
+end
